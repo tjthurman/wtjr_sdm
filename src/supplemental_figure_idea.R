9ffc99cacf857eec7157208f91129f6c63cab288
Tim <timothy.j.thurman@gmail.com>
2022-11-23 15:55:01 -0600

    update sampling map and raw data readme

--- src/supplemental_figure_idea.R
+++ src/supplemental_figure_idea.R
@@ -19,8 +19,6 @@ coast <- ne_coastline(scale = 10, returnclass = "sf")
 
 # Read in IUCN range ------------------------------------------------------
 iucn_range <- sf::read_sf("raw_data/redlist_species_data_79e8f518-a14c-4d0e-9640-5bea84d7c1b8/data_0.shp")
-plot(iucn_range[1])
-
 
 # Get SDM occurrences -----------------------------------------------------
 in_csv <- "processed_data/thin/wtjr_occ_0km_thin1.csv"
@@ -55,48 +53,13 @@ holes <- range_df %>%
 
 
 # get locations for all genetic samples -----------------------------------
-# Get samples from the bamlist
-seqed_samples <- read_delim(file = "../wtjr_genomics/data/all_wtjr_sample_bamlist.txt", delim = "\t", col_names = "bam_file") %>% 
-  mutate(sample = basename(bam_file)) %>% 
-  mutate(sample = str_remove(sample, ".realigned.sorted.bam")) %>% 
-  mutate(sample = str_remove(sample, "_realigned_sorted.bam"))
-
-
-# My data
-tim <- read_csv("../wtjr_genomics/data/labwork/possible_samples_to_extracts.csv") %>% 
-  select(sample = ID_museum, year, latitude, longitude, winter_pheno, state, source = source.x) %>% 
-  mutate(museum = year < 2008) %>% 
-  mutate(museum = ifelse(is.na(museum), T, museum)) %>% 
-  mutate(batch = "tim")
-
-# Fix the LTW_TWO_4140 sample
-tim$sample[tim$sample == "Mafalda_LTW_WYO_4140"] <- "LTW_COL_4140"
-tim$year[tim$sample == "LTW_COL_4140"] <- NA
-tim$latitude[tim$sample == "LTW_COL_4140"] <- 39.04814
-tim$longitude[tim$sample == "LTW_COL_4140"] <- -105.56139
-tim$winter_pheno[tim$sample == "LTW_COL_4140"] <- NA
-tim$state[tim$sample == "LTW_COL_4140"] <- "CO"
-tim$source[tim$sample == "LTW_COL_4140"] <- "LTW_COL_4140"
-
-
-# Mafalda's data
-mafalda <- read_excel("../wtjr_genomics/data/mf_thesis_table_S3_3.xlsx") %>% 
-  filter(Species == "Lepus townsendii") %>%
-  mutate(museum = ifelse(Origin == "Field", F, T),
-         year = NA, 
-         winter_pheno = NA,
-         sample = str_replace(`Sample ID`, "_", "-"),
-         batch = "mafalda") %>% 
-  separate(`Location (Population)`, into = c("country", "state"), sep = "/") %>% 
-  select(sample, year, latitude = `Lat.`, longitude = `Long.`,  winter_pheno, source = Origin, state, museum, batch) %>% 
-  bind_rows(., read_csv("../wtjr_genomics/data/mafalda_UCM_samples.csv"))
+sample_info <- read_csv("raw_data/genetics/samples_info.csv")
 
 # Data on which samples went into the 74 for GWAS
 disambig <- read_xlsx("../wtjr_genomics/data/WTJR_74lowcovsamples_code_disamb.xlsx") 
 
 
-genetic_samples <- seqed_samples %>% 
-  left_join(bind_rows(tim, mafalda)) %>% 
+genetic_samples <- sample_info %>% 
   select(sample, latitude, longitude) %>% 
   mutate(`Sample type` = ifelse(sample %in% str_replace(disambig$SampleID, "_", "-"), "WGS- GWAS", "WGS- other"))
 
@@ -120,7 +83,12 @@ ggplot() +
   geom_sf(data = state_prov, color = "grey51", fill = state_fill, size = 0.25) +
   geom_polygon(data = range_df, aes(x = long, y = lat, group = group, fill = name), color = sdm_border_col) + 
   geom_polygon(data = holes, aes(x = long, y = lat, group = group), fill = state_fill, color = sdm_border_col) + 
-  geom_point(data = to_plot, aes(x = longitude, y = latitude, color = `Sample type`), size = 1.5) +
+  scale_fill_manual(values = c("SDM range" = sdm_fill,
+                               "hole" = state_fill),
+                    limits = "SDM range") +
+  geom_point(data = to_plot, aes(x = longitude, y = latitude, 
+                                 color = `Sample type`, 
+                                 size = `Sample type`)) +
   geom_sf(data = state_prov, color = "grey51", fill = NA, size = 0.25) +
   geom_sf(data = countries, color = "grey10", fill = NA, size = 0.25) +
   geom_sf(data = coast, color = "grey10", fill = NA, size = 0.25) +
@@ -144,10 +112,12 @@ ggplot() +
         panel.border = element_rect(fill = NA, color = "black", size = 1.5),
         plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   scale_x_continuous(breaks = seq(-130, -89.2, by = 10)) +
-  scale_fill_manual(values = c("SDM range" = sdm_fill,
-                               "hole" = state_fill),
-                    limits = "SDM range") +
-  scale_color_okabe_ito() +
+  scale_size_manual(values = c("SDM- presence" = 0.75,
+                               "WGS- GWAS" = 2,
+                               "WGS- other" = 2)) +
+  scale_color_manual(values = c("SDM- presence" = palette_okabe_ito()[1],
+                                "WGS- GWAS" = palette_okabe_ito()[5],
+                                "WGS- other" = palette_okabe_ito()[3])) +
   guides(fill = guide_legend(title = NULL, title.position = "bottom", override.aes = list(color = sdm_border_col)),
          color = guide_legend(title.theme = element_text(size = 10)),
          linetype = guide_legend(title = NULL, title.position = "bottom")) +
@@ -155,6 +125,7 @@ ggplot() +
                  y.min = 32, y.max = 60.5, 
                  dist  = 300, dist_unit = "km", model = "WGS84", transform = T, 
                  anchor = c(x = -123.5, y = 33.5), st.size = 10/.pt, border.size = 0.25)
-ggsave("results/prelim_sampling_map.pdf")
+ggsave("results/figures/supplemental/sampling_map.pdf", width = 6.25, height = 6.25, units = "in")
+ggsave("results/figures/supplemental/sampling_map.jpeg", width = 6.25, height = 6.25, units = "in", dpi = 320)
 
 
